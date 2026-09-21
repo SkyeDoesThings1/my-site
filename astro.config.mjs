4644c71a6aae88e884391a896ce4a1b93607e7bd
Skyler <skyedoesthings1@gmail.com>
2026-09-21 14:58:01 -0400

    Feat: Add font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    fonts: [
+        {
+            provider: fontProviders.bunny(),
+            name: "M PLUS Rounded 1c",
+            cssVariable: "--font-m-plus-rounded-1c",
+            fallbacks: ["Arial", "Helvetica Neue", "Helvetica", "sans-serif"],
+            weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        },
+    ],
 });
