dc49a78f07869239a998a7fdacb8cc50d78ac3e8
Skyler <skyedoesthings1@gmail.com>
2026-09-21 21:46:36 -0400

    Style: Force pages to use trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    trailingSlash: "always",
     vite: {
         plugins: [tailwindcss()],
     },
