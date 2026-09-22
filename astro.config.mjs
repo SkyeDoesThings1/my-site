7cfc0b22e74c7d887641d4d88be55670b6c3df72
Skyler <skyedoesthings1@gmail.com>
2026-09-22 00:20:58 -0400

    Chore: Prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://skyedoesthings1.github.io/",
+    base: "/",
     trailingSlash: "always",
     output: "static",
     integrations: [icon()],
